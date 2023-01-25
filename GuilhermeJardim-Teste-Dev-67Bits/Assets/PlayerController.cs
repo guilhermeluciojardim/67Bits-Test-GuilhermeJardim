8d6b58387833fbe7a7015a2a0c63c1afdbc0ff15
guilhermeluciojardim <guilhermelucio@gmail.com>
2023-01-25 10:28:56 -0300

    Scripting commit
    - Created Player Controller
    - Created Camera Movement Controller
    - Created Basic Animations

---
+++ GuilhermeJardim-Teste-Dev-67Bits/Assets/PlayerController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Variables
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    public bool isAttacking;
+    private Vector3 moveDirection;
+    private Vector3 velocity;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
+    [SerializeField] private RuntimeAnimatorController newController;
+
+   //References
+    private CharacterController controller;
+    private Animator anim;
+
+    private void Start(){ 
+        controller = GetComponent<CharacterController>();
+        anim = GetComponent<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Update(){
+        Move();
+        Attack();
+    }
+    
+    private void Move(){
+
+        float moveZ = Input.GetAxis("Vertical");
+        float rotY = 3f * Input.GetAxis("Mouse X");
+
+        transform.Rotate(0,rotY,0);
+        
+        moveDirection = new Vector3(0,0,moveZ);
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        anim.SetBool("isMoving",true);
+            
+            if ((moveDirection != Vector3.zero) && (!Input.GetKey(KeyCode.LeftShift))){
+                    WalkAnim();
+            }
+            else if ((moveDirection != Vector3.zero) && (Input.GetKey(KeyCode.LeftShift)) && (moveZ > 0)){
+                RunAnim();
+            }
+            else if (moveDirection == Vector3.zero){
+                IdleAnim();
+            }
+            moveDirection *= moveSpeed;
+
+        controller.Move(moveDirection * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Attack(){
+            if (Input.GetKeyDown(KeyCode.Mouse0)){
+                    anim.SetBool("Attack1",true);
+                    StartCoroutine(WaitForNextAttack());   
+        }
+    }
+    IEnumerator WaitForNextAttack(){
+        yield return new WaitForSeconds(0.5f);
+        anim.SetBool("Attack1",false);
+        isAttacking=false;
+    }
+    private void IdleAnim(){
+        anim.SetFloat("Speed", 0,0.1f,Time.deltaTime);
+    }
+    private void WalkAnim(){
+        moveSpeed = walkSpeed;
+        anim.SetFloat("Speed", 0.5f,0.1f,Time.deltaTime);
+    }
+    private void RunAnim(){
+        moveSpeed = runSpeed;
+        anim.SetFloat("Speed", 1f,0.1f,Time.deltaTime);
+    }
+}
